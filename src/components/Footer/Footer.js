7ac3766c13c86d14e6c08033d4eb2444c8b1ce13
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-08-12 17:58:19 -0700

    add footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react"
+import styles from "./Footer.module.scss"
+
+export default function Footer() {
+  return (
+    <div className={styles.Footer}>
+      Footer <p>Attribute illustration author here</p>
+    </div>
+  )
+}
