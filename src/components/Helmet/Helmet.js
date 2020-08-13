17eacc36e576d02c33e2115330ce783d3089ef2f
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-08-12 22:38:05 -0700

    fix helmet name clash

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Helmet from "react-helmet"
 
-export default function Helmet() {
+export default function Head() {
   return (
     <Helmet>
       <title>Midway Web Development</title>
