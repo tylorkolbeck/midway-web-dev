9764431d08019c9c2b9bb5502e94a3b2004985c4
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-08-12 17:59:33 -0700

    add icons to button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,10 @@
 import React from "react"
 import styles from "./Button.module.scss"
+import { FiArrowRight } from "react-icons/fi"
+
+let iconMapping = {
+  arrowRight: <FiArrowRight />,
+}
 
 export default function Button(props) {
   return (
@@ -8,6 +13,7 @@ export default function Button(props) {
       className={[styles.Button, styles[props.bg]].join(" ")}
     >
       {props.children}
+      {props.icon ? iconMapping[props.icon] : null}
     </button>
   )
 }
