b12e84e873d2464f87e7773263ba88d3387e3b9c
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-08-12 21:59:33 -0700

    add form submit to netlify

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,27 +1,80 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styles from "./ContactForm.module.scss"
 import Button from "../Button/Button"
 
+const initialFormState = {
+  name: "",
+  email: "",
+  message: "",
+}
+
 export default function ContactForm(props) {
+  const [state, setState] = useState(initialFormState)
+  const [error, setError] = useState(false)
+
   let formClass = props.mini ? styles.miniForm : ""
 
+  const encode = data => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&")
+  }
+
+  function handleFormSubmit(e) {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...state }),
+    })
+      .then(() => {
+        setState(initialFormState)
+        setError(false)
+      })
+      .catch(() => setError("There was an error sending your message."))
+
+    e.preventDefault()
+  }
+
+  function handleFormChange(e) {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
   return (
-    <form name="contact" method="POST" data-netlify="true">
+    <form onSubmit={handleFormSubmit}>
       <div>
         <h1>Contact Us</h1>
+        <p className="error-text">{error && error}</p>
       </div>
+
       <div className={[styles.ContactForm, formClass].join(" ")}>
         <div>
           <h4>Name</h4>
-          <input placeholder="Enter your name" type="text" name="name" />
+          <input
+            placeholder="Enter your name"
+            type="text"
+            name="name"
+            value={state.name}
+            onChange={handleFormChange}
+          />
         </div>
         <div>
           <h4>Email</h4>
-          <input placeholder="Enter your email" type="email" name="email" />
+          <input
+            placeholder="Enter your email"
+            type="email"
+            name="email"
+            value={state.email}
+            onChange={handleFormChange}
+          />
         </div>
         <div className={styles.ContactForm_textarea}>
           <h4>Message</h4>
-          <textarea placeholder="Enter your messsage..." name="message" />
+          <textarea
+            placeholder="Enter your messsage..."
+            name="message"
+            value={state.message}
+            onChange={handleFormChange}
+          />
         </div>
         <div>
           <Button
