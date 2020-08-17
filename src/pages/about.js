13577b3b223f86c67117fbebc96ab91e95dc895c
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-08-16 18:20:30 -0700

    add copy

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,9 @@ import Layout from "../components/layout"
 import Hero from "../components/Hero/Hero"
 import { Card } from "../components/Card/Card"
 import avatar from "../assets/avatar_default.png"
+import tylorImg from "../assets/tylor_cropped.png"
+import johnImg from "../assets/john_cropped.png"
+import kailaImg from "../assets/kaila_cropped.png"
 // import {
 //   AiOutlineArrowRight,
 //   AiFillFacebook,
@@ -24,30 +27,32 @@ export default function About({ data }) {
         <div className={styles.Cards_people}>
           <Card>
             <Card.Image size="80%">
-              <img src={avatar} alt="Profile 1" />
+              <img src={tylorImg} alt="Profile 1" />
             </Card.Image>
             <Card.Title position="center">Tylor Kolbeck</Card.Title>
-            <Card.SubTitle position="center">Web Developer</Card.SubTitle>
+            <Card.SubTitle position="center">Web | UX Developer</Card.SubTitle>
             <div style={{ textAlign: "center" }}>
               <Card.Link>Learn More</Card.Link>
             </div>
           </Card>
           <Card>
             <Card.Image size="80%">
-              <img src={avatar} alt="Profile 1" />
+              <img src={johnImg} alt="Profile 1" />
             </Card.Image>
             <Card.Title position="center">John Kaplanis</Card.Title>
-            <Card.SubTitle position="center">Web Developer</Card.SubTitle>
+            <Card.SubTitle position="center">
+              Web Developer | Sales
+            </Card.SubTitle>
             <div style={{ textAlign: "center" }}>
               <Card.Link>Learn More</Card.Link>
             </div>
           </Card>
           <Card>
             <Card.Image size="80%">
-              <img src={avatar} alt="Profile 1" />
+              <img src={kailaImg} alt="Profile 1" />
             </Card.Image>
             <Card.Title position="center">Kaila Kolbeck</Card.Title>
-            <Card.SubTitle position="center">Graphic Designer</Card.SubTitle>
+            <Card.SubTitle position="center">Designer</Card.SubTitle>
             <div style={{ textAlign: "center" }}>
               <Card.Link>Learn More</Card.Link>
             </div>
@@ -56,18 +61,20 @@ export default function About({ data }) {
         <aside className={styles.Aside_about}>
           <h1 style={{ textAlign: "center" }}>Our Mission</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-            perferendis itaque rerum nam voluptatum est in explicabo reiciendis
-            minima vel. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Porro perferendis itaque rerum nam voluptatum est in explicabo
-            reiciendis minima vel.
+            Our mission is to make the planning and deployment of your next web
+            project a fun and painless process. Your website defines what your
+            company is what it is about so it should be nothing short of an
+            exciting process.
           </p>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima
-            deleniti placeat iure harum nihil officia dolore, est ipsum? Labore,
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-            perferendis itaque rerum nam voluptatum est in explicabo reiciendis
-            minima vel. ab.
+            While you handle the aspects of starting or running your business,
+            let us handle getting you online so that the world can discover you.
+            Our timely and cost effective solutions will give you one less thing
+            to worry about.
+          </p>
+          <p>
+            We proudly stand by our services and deliver our products with a
+            100% satisfaction gurantee.{" "}
           </p>
         </aside>
       </section>
--- src/pages/service-webdevelopment.js
+++ src/pages/service-webdevelopment.js
@@ -14,93 +14,87 @@ export default function Webdevelopment() {
         bg="bg_tert"
         size="half"
         heading="Web Development Services"
-        subText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, vitae?"
+        subText="At Midway Web Development we have have a team of proffesionals that
+        can provide you with all of the necessary services to get your
+        business online."
       />
-
-      <section>
-        <Aside>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat
-          officiis expedita doloremque id deserunt praesentium provident
-          laboriosam suscipit ex cum?
-        </Aside>
-      </section>
-
       <section className={styles.Technologies}>
         <h1>Technologies</h1>
+        <p>
+          These are just some of the technologies that we specialize in and use
+          to build your website.
+        </p>
         <WhatWeKnow />
       </section>
-
       <section className={styles.Content}>
+        {/* <Aside>
+          We believe that getting your website up and running should be an
+          enjoyable process because it will define who you are on the web.{" "}
+        </Aside> */}
         <h1>About Our Services</h1>
         <div>
-          <h2>Lorem, ipsum dolor.</h2>
+          <h2>Qualility Online Presence</h2>
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis unde
-            sed possimus dolorem aliquid eaque doloribus tenetur saepe
-            necessitatibus quae. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Quidem, fuga aut. Commodi dolores optio veritatis
-            nemo ratione hic natus odit?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            eaque aperiam illo similique nobis mollitia dolore labore neque!
-            Facilis, explicabo?
+            The easiest way for a company to legitimize themselves and build
+            client trust is to have a strong internet presence. An online
+            presence not only adds value to your companies brand but it also
+            offers an opportunity get your business in front of possible
+            clients. A website not only consist of having flashy graphics and
+            colors, but of thoughtful design and usability. At MWD we take
+            usability very seriously. If you have a product it will not be worth
+            a penny if it is not usable.
           </p>
         </div>
       </section>
 
       <section className={styles.Content}>
         <div>
-          <h2>Lorem, ipsum dolor.</h2>
+          <h2>Responsive Web Design</h2>
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis unde
-            sed possimus dolorem aliquid eaque doloribus tenetur saepe
-            necessitatibus quae. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Quidem, fuga aut. Commodi dolores optio veritatis
-            nemo ratione hic natus odit?
+            With over half of all web traffic coming from a mobile device it is
+            important that your website looks good and performs well across as
+            many devices sizes as possible. Thats why at MWD we develop websites
+            that are responsive and performant by using the latest in modern web
+            technologies.
           </p>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            eaque aperiam illo similique nobis mollitia dolore labore neque!
-            Facilis, explicabo?
+            We first design your website with the mobile user in mind by
+            optimizing media to be delivered on slow speed connections as well
+            ensuring the layout works for a smaller device. We then design out
+            your desktop sized site taking advantage of the extra screen real
+            estate and connection performance to really wow your audience.
           </p>
         </div>
       </section>
 
-      <Hero bg="bg_tert" size="quarter" heading="Web Development Services" />
+      {/* <Hero
+        bg="bg_tert"
+        size="quarter"
+        heading="50% of web traffic is from a mobile device. "
+      /> */}
 
       <section className={styles.Content}>
-        <h1>Why Websites Are Important</h1>
         <div>
-          <h2>Lorem, ipsum dolor.</h2>
+          <h2>SEO And Marketing</h2>
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis unde
-            sed possimus dolorem aliquid eaque doloribus tenetur saepe
-            necessitatibus quae. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Quidem, fuga aut. Commodi dolores optio veritatis
-            nemo ratione hic natus odit?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            eaque aperiam illo similique nobis mollitia dolore labore neque!
-            Facilis, explicabo?
+            SEO or “Search Engine Optimization” is the practice of increasing
+            traffic to your website through search engine results. With quality
+            SEO you can expect to attract visitors to your site that are looking
+            for your service and potentially converting that user into a future
+            client. All of our websites are made with SEO as a top priority,
+            there is no point in having a website if people can not find it
           </p>
         </div>
       </section>
       <section className={styles.Content}>
         <div>
-          <h2>Lorem, ipsum dolor.</h2>
+          <h2>Website Content</h2>
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis unde
-            sed possimus dolorem aliquid eaque doloribus tenetur saepe
-            necessitatibus quae. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Quidem, fuga aut. Commodi dolores optio veritatis
-            nemo ratione hic natus odit?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            eaque aperiam illo similique nobis mollitia dolore labore neque!
-            Facilis, explicabo?
+            A website is not useful if it does not provide the right message.
+            Your site “copy” is the content that delivers your message and
+            guides people through your website to convey information and deliver
+            your message. At MWD we help you to create your message so that your
+            can convert visitors into paying clients.
           </p>
         </div>
       </section>
