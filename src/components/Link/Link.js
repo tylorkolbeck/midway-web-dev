3886d800ec5ab760610ff16d461d42728a0a220a
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-08-18 22:15:47 -0700

    make default link color tertiary color

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -3,7 +3,9 @@ import styles from "./Link.module.scss"
 import { Link } from "gatsby"
 
 export default function LinkEl({ to, children, color }) {
-  const classNames = [color ? styles[color] : styles.pri, styles.Link].join(" ")
+  const classNames = [color ? styles[color] : styles.tert, styles.Link].join(
+    " "
+  )
 
   return (
     <Link to={to} className={classNames}>
