1011672e6967445573c1a99b0963d361b8b4ad57
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-08-24 13:36:11 -0700

    update meta data

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -9,7 +9,7 @@ export default function Head() {
       <meta name="title" content="Midway Web Development" />
       <meta
         name="description"
-        content="Midway Web Development is a team of web developers that are here to get your business on line, whether its a company website or a web application, we can make it happen."
+        content="Midway Web Development provides modern and efficient web, UI/UX, and design services."
       />
 
       <meta property="og:type" content="website" />
@@ -17,17 +17,17 @@ export default function Head() {
       <meta property="og:title" content="Midway Web Development" />
       <meta
         property="og:description"
-        content="Midway Web Development is a team of web developers that are here to get your business on line, whether its a company website or a web application, we can make it happen."
+        content="Midway Web Development provides modern and efficient web, UI/UX, and design services."
       />
-      <meta property="og:image" content="../../assets/meta_image.png" />
-
+      <meta property="og:image" content="../static/homepage.png" />
+      {/* 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://metatags.io/" />
-      <meta property="twitter:title" content="Midway Web Development" />
-      <meta
+      <meta property="twitter:title" content="Midway Web Development" /> */}
+      {/* <meta
         property="twitter:description"
         content="Midway Web Development is a team of web developers that are here to get your business on line, whether its a company website or a web application, we can make it happen."
-      />
+      /> */}
       <meta
         property="twitter:image"
         content="../../assets/meta_image.png"
